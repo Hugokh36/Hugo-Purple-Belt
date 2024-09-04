2a3ae78f1391f6a948f13ee5dede5514e8251401
MasterHacker10 <>
2024-09-04 15:56:22 -0400

    Step 45
    Step 45

--- Dropping Bombs Part3/Assets/Scripts/GameManager.cs
+++ Dropping Bombs Part3/Assets/Scripts/GameManager.cs
@@ -5,23 +5,45 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private Spawner spawner;
+    public GameObject title;
+    private Vector2 screenBounds;
+    public GameObject playerPrefab;
+    private GameObject player;
+    private bool gameStarted = false;
 
     void Awake()
     {
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        player = playerPrefab;
     }
     // Start is called before the first frame update
     void Start()
     {
         spawner.active = false;
+        title.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-      if (Input.anyKeyDown)
+        if (!gameStarted)
         {
-            spawner.active = true;
+          if (Input.anyKeyDown)
+          {
+                ResetGame();           
+          }                               
+        }             
+      
+
+        var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
+
+        foreach (GameObject bombObject in nextBomb)
+        {
+            if (bombObject.transform.position.y < (-screenBounds.y) - 12)
+            {
+                Destroy(bombObject);
+            }
         }
     }
 }
